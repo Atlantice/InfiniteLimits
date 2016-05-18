d8d00a4a46ded62d33b060d966e499029373b710
Atlantice <pereiram13@students.ecu.edu>
2016-05-17 20:12:53 -0400

    Add files via upload

--- includes/form_1.php
+++ includes/form_1.php
@@ -8,13 +8,13 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	
-	$to = 'brittany@infinitelimitstutoring.com'; // Email submissions are sent to this email
+	$to = 'accounts@infinitelimitstutoring.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from ilt.";
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nEmail: $email \nMessage: $message \n";
-	$headers = "From: brittany@infinitelimitstutoring.com\n";
+	$headers = "From: accounts@infinitelimitstutoring.com\n";
 	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
